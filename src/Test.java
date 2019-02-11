3fb2b0777ae819886b6c9fa2f9b9d8839c9b82bc
taradaszje <j.sularz@gmail.com>
2019-02-11 10:19:58 +0100

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Test {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new CalendarView();
+            }
+        });
+
+
+    }
+}
